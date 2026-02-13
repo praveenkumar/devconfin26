7cd24bfb1824d25836111a58c94fc7721e25bb14
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2026-02-13 22:10:21 +0530

    Remove registry part

--- scripts/load_embedded_images.sh
+++ scripts/load_embedded_images.sh
@@ -7,8 +7,3 @@ echo "Loading Gitea image into container-storage"
 skopeo copy \
   dir:/usr/lib/containers-image-cache/gitea \
   containers-storage:docker.io/gitea/gitea:1.25
-
-echo "Loading Registry image into container-storage"
-skopeo copy \
-  dir:/usr/lib/containers-image-cache/registry \
-  containers-storage:docker.io/registry:3.0
